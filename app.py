475adaa742b899acf582a18a32d05b9997b7be01
Uncle Teslim <teslimolukazeem@gmail.com>
2025-06-30 16:21:51 +0100

    readme

--- app.py
+++ app.py
@@ -84,6 +84,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
-else:
-    pass
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
